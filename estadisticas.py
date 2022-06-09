026b7215d1bc096611c939907e0b7403ba4764bc
saulrega <p4n73ra@gmail.com>
2022-06-09 12:32:23 -0500

    Create repository

---
+++ estadisticas.py
@@ -0,0 +1,10 @@
+import random
+
+def media(X):
+    return sum(X) / len(X)
+
+if __name__ == '__main__':
+    X = [ random.randint(1, 21) for i in range(20) ]
+    mu = media(X) #Aqui
+    print(X)
+    print(f'Media: {mu}')
